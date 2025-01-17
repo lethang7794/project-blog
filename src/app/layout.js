814c375c1e00cb3313ef8b45b96f7423c3f83c2d
Le Quang Thang <lethang7794@gmail.com>
2025-01-17 14:59:49 +0700

    extra builder info into plugin

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,11 +26,7 @@ const monoFont = Spline_Sans_Mono({
 
 export const metadata = {
   other: {
-    env: process.env.BUILD_ENV ||process.env.GIT_BRANCH,
-    version: [process.env.GIT_COMMIT_TAG, process.env.GIT_COMMIT_HASH, process.env.GIT_COMMIT_DATE]
-      .filter(Boolean)
-      .join(' - '),
-    'deployed-at': process.env.BUILD_DATE,
+    ...getBuildMetadata(),
   },
 };
 
